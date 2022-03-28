5a293f8f489978a08153871c40a81828ea2c548c
A-Doubt <adoubt2@gmail.com>
2022-03-29 00:45:56 +0200

    Added: Die component, Die roll function, Die state and ability to reroll all dice.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import Die from "./components/Die";
+import "./index.css"
+import React from "react";
 
 function App() {
+
+	const [dice, setDice] = React.useState(allNewDice())
+
+	function allNewDice() {
+		const diceArray = []
+		for (let i = 0; i < 10; i++) {
+			diceArray.push(Math.ceil(Math.random() * 6));
+		}
+		return diceArray;
+	};
+	function rollDice() {
+		setDice(allNewDice());
+	}
+
+	const diceElements = dice.map((die, index) => {
+		return <Die value={die} />
+	})
+
 	return (
-		null
+		<main>
+			<h1 className='game--h1'>Tenzies game</h1>
+			<div className='dice'>
+				{diceElements}
+			</div>
+			<button 
+				className='dice--button'
+				onClick={rollDice}
+			>
+				Roll dice
+			</button>
+		</main>
 	);
 }
 export default App;
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+export default function Die(prop) {
+	return (
+		<div className='die'>
+			<h3 className='die--value'>{prop.value}</h3>
+		</div>
+	)
+}
